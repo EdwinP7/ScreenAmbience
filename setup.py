bbe0709299f80c9bc6de567c172e169133754caf
Edwin P <edwinperea7@gmail.com>
2018-04-29 18:17:59 -0700

    setup config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup (
     name = 'ScreenAmbience',
-    version = '1.0.5',
+    version = '1.0.6',
     description = 'Matches zones on a LIFX-Z strip to the colors on your screen.',
     url = 'https://github.com/EdwinP7/ScreenAmbience',
     author = 'Edwin Perea',
